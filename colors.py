50c41d06e544c7d81e265692c94df124e7deff93
Meluiscruz <info@meluiscruz.com>
2021-04-22 01:50:51 -0500

    End of the project

---
+++ colors.py
@@ -0,0 +1,25 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#8eaba8"
+SCORE_LABEL_FONT = ("Verdana", 40, "bold")
+SCORE_FONT = ("Helvetica", 36, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc000"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {2: "#daeddf", 4: "#9ae3ae", 8: "#6ce68d", 16: "#42ed71",
+                   32: "#17e650", 64: "#17c246", 128: "#149938",
+                   256: "#107d2e", 512: "#0e6325", 1024: "#0b4a1c",
+                   2048: "#031f0a",}
+
+CELL_NUMBER_COLORS = {2: "#011c08", 4: "#011c08", 8: "#011c08", 16: "#011c08",
+                   32: "#011c08", 64: "#f2f2f0", 128: "#f2f2f0",
+                   256: "#f2f2f0", 512: "#f2f2f0", 1024: "#f2f2f0",
+                   2048: "#f2f2f0",}
+
+CELL_NUMBER_FONTS = {2: ("Helvetica", 55, "bold"), 4: ("Helvetica", 55, "bold"),
+                     8: ("Helvetica", 55, "bold"), 16: ("Helvetica", 50, "bold"),
+                     32: ("Helvetica", 50, "bold"), 64: ("Helvetica", 50, "bold"),
+                     128: ("Helvetica", 45, "bold"), 256: ("Helvetica", 45, "bold"),
+                     512: ("Helvetica", 45, "bold"), 1024: ("Helvetica", 40, "bold"),
+                     2048: ("Helvetica", 40, "bold"),}
